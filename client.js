909e498522eeef720c39b6d557b2925b98195025
Jamie Richison <jamierichi@yahoo.com>
2019-11-08 16:23:34 -0600

    logic complete for most core elements. functions created include: onReady, addEmployee, showEmployees, removeEmployee, and calculateMonthlySalary

---
+++ client.js
@@ -0,0 +1,78 @@
+$(document).ready(onReady);
+
+function onReady(){
+    $('#add-employee-btn').on('click', addEmployee);
+    $( '#employee-table' ).on('click', '.delete', removeEmployee, )
+    calculateMonthlySalary();
+}
+
+let employees = [];
+
+function addEmployee(event){
+    //prevent page from refreshing on submit button
+    event.preventDefault();
+    //declare variables for inputs
+    let fName = $( '#first-name-in' ).val();
+    let lName = $( '#last-name-in' ).val();
+    let idNumber = $( '#id-in' ).val();
+    let jobTitle = $( '#job-title-in' ).val();
+    let annualSalary = $( '#annual-salary-in' ).val();
+
+    //push new employee object to the employees array
+    employees.push(
+        {
+            fName: fName,
+            lName: lName,
+            idNumber: idNumber,
+            jobTitle: jobTitle,
+            annualSalary: annualSalary
+        }
+    )
+    //clear out the inputs
+    $( '#first-name-in' ).val('');
+    $( '#last-name-in' ).val('');
+    $( '#id-in' ).val('');
+    $( '#job-title-in' ).val('');
+    $( '#annual-salary-in' ).val('');
+    // call function to show employees in the table
+    showEmployees( employees );
+    calculateMonthlySalary();
+}
+function showEmployees( array ){
+    //empty the table
+    $( '#employee-table').empty();
+    //loop for each employee in the array and 
+    //add a new <tr> and <td> for each category
+    for ( let employee of array ){
+        $( '#employee-table' ).append(
+            `<tr>
+                <td>${employee.fName}</td>
+                <td>${employee.lName}</td>
+                <td>${employee.idNumber}</td>
+                <td>${employee.jobTitle}</td>
+                <td>${employee.annualSalary}</td>
+                <td><button class="delete">Delete</button></td>
+            </tr>`);
+    }
+}
+
+function removeEmployee(){
+    //declare button variable to this
+    let button = $( this );
+    //removes tr element
+    button.closest('tr').remove();
+}
+
+function calculateMonthlySalary(){
+    let totalSalary = 0;
+    //loop through employees array and add total of all annual salaries
+    for ( let i=0; i < employees.length; i++){
+        totalSalary += Number( employees[i].annualSalary );
+    }// end loop
+    // divide totalSalary by 12 to get monthly salary
+    const monthlySalary = totalSalary / 12;
+    //display monthly budget
+    let monthlyBudget = $( '#monthlyout' );
+    monthlyBudget.empty();
+    monthlyBudget.append( monthlySalary );
+}
